64082a79d42709c578d9917a2dd80e452f26a778
Jonathan Salazar <jonyalex1047@gmail.com>
2025-10-09 20:55:51 -0500

    fix: env var name for messages table changed

--- lambda/contact_us/lambda_function.py
+++ lambda/contact_us/lambda_function.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 region_name = 'us-east-1'
 bucket_name = os.environ['BUCKET_NAME']
-table_name = os.environ['TABLE_NAME']
+table_name = os.environ['MESSAGES_TABLE_NAME']
 
 s3 = boto3.client('s3', region_name=region_name)
 db = boto3.resource('dynamodb', region_name=region_name)
